c246d15e280722f908e866cc90d03dd144637a93
voice49 <55628323+voice49@users.noreply.github.com>
2024-07-03 22:13:55 +0300

    Add files via upload

--- rofipower/rofipower.sh
+++ rofipower/rofipower.sh
@@ -10,22 +10,33 @@ log_out="  log out"
 # Variable passed to rofi
 options="$power_off\n$reboot\n$lock\n$suspend\n$log_out"
 
-chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 2)"
+chosen=$(echo -e "$options" | $rofi_command -dmenu -selected-row 2 -p "Powermenu")
 case $chosen in
     $power_off)
+        run ffplay -nodisp -autoexit  $HOME/Music/logout.wav
+        sleep 3
         systemctl poweroff
         ;;
     $reboot)
+        run ffplay -nodisp -autoexit  $HOME/Music/device-removed.oga
+        sleep 3
         systemctl reboot
         ;;
     $lock)
-        betterlockscreen  -l dimblur         #  betterlockscreen [-u <PATH>] [-l <EFFECT>] [-w <EFFECT>] --show-layout#
-
+        run ffplay -nodisp -autoexit  $HOME/Music/alert.wav
+        sleep 3
+        dm-tool lock
+         #1#scrot /tmp/screen.png
+         #1#xwobf -s 11 /tmp/screen.png
+         #1#i3lock -i /tmp/screen.png
+         #1#rm /tmp/screen.png
         ;;
     $suspend)
         systemctl hibernate
         ;;
     $log_out)
+        run ffplay -nodisp -autoexit  $HOME/Music/logout.wav
+        sleep 3
         xdotool key --clearmodifiers super+shift+x exit
         ;;
 esac
